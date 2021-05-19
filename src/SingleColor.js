7a83e91e33b664ba7026d3e9b3e7a4291763f07d
Ruslan <rus@gmx.com>
2021-05-19 18:15:17 +0100

    Set alert state

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils';
 
 const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
   return <h4>single color</h4>;
 };
 
