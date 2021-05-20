d8b4b9d9bb75c54411d2536699760d701a38433b
Ruslan <rus@gmx.com>
2021-05-20 23:51:43 +0100

    Refactor hex finder

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -4,12 +4,12 @@ import rgbToHex from './utils';
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb);
+  // const hex = rgbToHex(...rgb);
 
   return (
     <article className={`color`} style={{ backgroundColor: `rgb(${bcg})` }}>
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">{hex}</p>
+      <p className="color-value">{hexColor}</p>
     </article>
   );
 };
