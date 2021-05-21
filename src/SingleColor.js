50daa229f21549d80b206b84c7e7a020978f95a1
Ruslan <rus@gmx.com>
2021-05-21 23:57:30 +0100

    Add ability to copy to clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -4,13 +4,17 @@ import rgbToHex from './utils';
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb);
+  // const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
 
   return (
     <article
       className={`color ${index > 10 && 'color-light'}`}
-      style={{ backgroundColor: `rgb(${bcg})` }}>
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}>
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hexValue}</p>
 
